1bf3371b5c4e42a90cadbfd609499c2d02a2ab72
Etienne Warneck <etienne.warneck@gmail.com>
2020-08-11 17:39:32 -0400

    changing styles Home and Github

--- src/components/NavBarTitle/NavBarTitle.js
+++ src/components/NavBarTitle/NavBarTitle.js
@@ -5,12 +5,12 @@ import classes from './NavBarTitle.module.css';
 
 class NavBarTitle extends Component {
 
-    
+
 
     render() {
 
         const styleHomeButton = {
-            border: '1px solid red',
+            // border: '1px solid red',
             color: 'black',
             fontSize: '1.2em',
             marginLeft: '40%',
@@ -21,7 +21,7 @@ class NavBarTitle extends Component {
             boxShadow: 'none !important',
         }
         const styleHomeButton2 = {
-            border: '1px solid red',
+            // border: '1px solid red',
             color: 'black',
             fontSize: '1.2em',
             marginLeft: '50%',
@@ -36,10 +36,8 @@ class NavBarTitle extends Component {
                 <Navbar className={classes.style} bg="" expand="lg" color=" #DDC59D">
                     <Nav.Link className={classes.styleText} href='/'>Nobel Prize Laureates</Nav.Link>
                 </Navbar>
-                {/* <div >
+                <div >
                     <Nav.Link style={styleHomeButton} href="/">Home</Nav.Link>
-                </div> */}
-                <div>
                     <Nav.Link style={styleHomeButton2} href="https://github.com/EtienneWarneck/NobelPrizeAPI/tree/master/my-app">GitHub</Nav.Link>
                 </div>
             </div>
