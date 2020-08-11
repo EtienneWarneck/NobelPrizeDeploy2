6aef5c857dcab3ac865be749ce0dae46f999e81d
Etienne Warneck <etienne.warneck@gmail.com>
2020-08-11 17:09:41 -0400

    Every laureate has a working card

--- src/components/NavBarTitle/NavBarTitle.js
+++ src/components/NavBarTitle/NavBarTitle.js
@@ -5,12 +5,49 @@ import classes from './NavBarTitle.module.css';
 
 class NavBarTitle extends Component {
 
+    
+
     render() {
+
+        const styleHomeButton = {
+            // border: '1px solid red',
+            color: 'black',
+            fontSize: '1.2em',
+            // textAlign: 'left',
+            marginLeft: '40%',
+            marginRight: '10px',
+            position: 'absolute',
+            top: '70%',
+            // width: '100%',
+            outline: 'none !important',
+            boxShadow: 'none !important',
+        }
+        const styleHomeButton2 = {
+            // border: '1px solid red',
+                    // display: 'block',
+    
+            color: 'black',
+            fontSize: '1.2em',
+            // textAlign: 'left',
+            marginLeft: '50%',
+            position: 'absolute',
+            top: '70%',
+            // width: '100%',
+            outline: 'none !important',
+            boxShadow: 'none !important',
+        }
+        
         return (
             <div>
                 <Navbar className={classes.style} bg="" expand="lg" color=" #DDC59D">
-                        <Nav.Link className={classes.styleText} href='/'>Nobel Prize Laureates</Nav.Link>
+                    <Nav.Link className={classes.styleText} href='/'>Nobel Prize Laureates</Nav.Link>
                 </Navbar>
+                <div >
+                    <Nav.Link style={styleHomeButton} href="/">Home</Nav.Link>
+                </div>
+                <div>
+                    <Nav.Link style={styleHomeButton2} href="https://github.com/EtienneWarneck/NobelPrizeAPI/tree/master/my-app">GitHub</Nav.Link>
+                </div>
             </div>
         )
     }
