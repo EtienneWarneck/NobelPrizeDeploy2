e178f161fa3a36495d9b73ff3c90d69ca465306c
Etienne Warneck <etienne.warneck@gmail.com>
2020-08-12 08:54:18 -0400

    changing styles Home and Github

--- src/components/NavBarTitle/NavBarTitle.js
+++ src/components/NavBarTitle/NavBarTitle.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import classes from './NavBarTitle.module.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class NavBarTitle extends Component {
 
@@ -10,38 +10,14 @@ class NavBarTitle extends Component {
 
     render() {
 
-        const styleHomeButton = {
-            border: '1px solid red',
-            color: 'black',
-            fontSize: '1.2em',
-            marginLeft: '40%',
-            marginRight: '10px',
-            top: '70%',
-            width: '100px',
-            outline: 'none !important',
-            boxShadow: 'none !important',
-        }
-        const styleHomeButton2 = {
-            border: '1px solid red',
-            color: 'black',
-            fontSize: '1.2em',
-            // marginLeft: '50%',
-            top: '70%',
-            width: '100px',
-            outline: 'none !important',
-            boxShadow: 'none !important',
-        }
-
         return (
             <div>
                 <Navbar className={classes.style} bg="" expand="lg" color=" #DDC59D">
                     <Nav.Link className={classes.styleText} href='/'>Nobel Prize Laureates</Nav.Link>
                 </Navbar>
-                <div >
-                    <Link style={styleHomeButton} href="/">Home</Link>
-                    <Link style={styleHomeButton2} href="https://github.com/EtienneWarneck/NobelPrizeAPI/tree/master/my-app">GitHub</Link>
-                </div>
-            </div>
+                <Link style={classes.styleHomeButton} href="/">Home</Link>
+                <Link style={classes.styleHomeButton2} href="https://github.com/EtienneWarneck/NobelPrizeAPI/tree/master/my-app">GitHub</Link>
+            </div >
         )
     }
 }
