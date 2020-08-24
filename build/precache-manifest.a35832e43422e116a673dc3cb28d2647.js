278718a301403bcd4e5427d1fb7757fd6939dd48
Etienne Warneck <etienne.warneck@gmail.com>
2020-08-24 15:54:37 +0200

    npm run build

---
+++ build/precache-manifest.a35832e43422e116a673dc3cb28d2647.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1a60dc89a892598f02b717d60f7899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7b86300bf13c5ae06d3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1ac87c0a9f51dd0e0080",
+    "url": "/static/css/main.df526a03.chunk.css"
+  },
+  {
+    "revision": "f7b86300bf13c5ae06d3",
+    "url": "/static/js/2.e71cd1df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e71cd1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ac87c0a9f51dd0e0080",
+    "url": "/static/js/main.b714c1ea.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
