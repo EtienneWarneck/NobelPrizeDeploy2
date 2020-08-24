be6387ccde48efb3ee065918763e03a4d0918d94
Etienne Warneck <etienne.warneck@gmail.com>
2020-08-24 18:33:38 +0200

    styling Navigation NAv bootstrap or Nav react-router

--- src/components/NavBarTitle/NavBarTitle.js
+++ src/components/NavBarTitle/NavBarTitle.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 // import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import classes from './NavBarTitle.module.css';
-import { Link } from "react-router-dom";
+// import { Link, Nav } from "react-router-dom";
+import Nav from 'react-bootstrap/Nav'
+
 
 class NavBarTitle extends Component {
     render() {
@@ -54,12 +56,11 @@ class NavBarTitle extends Component {
 
         return (
             <div style={{ display: 'block', width:'100%'}}>
-                <Navbar className={classes.style} bg="" expand="lg" color=" #DDC59D">
-                    <Link className={classes.styleText} href='/'>Nobel Prize Laureates</Link>
-                </Navbar>
+                {/* <Navbar className={classes.style} bg="" expand="lg" color=" #DDC59D"> */}
+                    <Nav.Link className={classes.styleText} href='/'>Nobel Prize Laureates</Nav.Link>
                 <div style={{ display: 'block', width:'100%' }}>
-                    <Link style={styleHome} href="/">Home</Link>
-                    <Link style={styleGithub} href="https://github.com/EtienneWarneck/NobelPrizeAPI/tree/master/my-app">GitHub</Link>
+                    <Nav.Link style={styleHome} href="/">Home</Nav.Link>
+                    <Nav.Link style={styleGithub} href="https://github.com/EtienneWarneck/NobelPrizeAPI/tree/master/my-app">GitHub</Nav.Link>
                 </div>
             </div>
         )
