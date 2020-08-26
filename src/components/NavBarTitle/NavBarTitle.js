17256bec0137147347ada3bd6180e9e41e89e001
Etienne Warneck <etienne.warneck@gmail.com>
2020-08-26 17:13:01 +0200

    Footer

--- src/components/NavBarTitle/NavBarTitle.js
+++ src/components/NavBarTitle/NavBarTitle.js
@@ -8,26 +8,7 @@ import Nav from 'react-bootstrap/Nav'
 
 class NavBarTitle extends Component {
     render() {
-        // const styleHomeButton = {
-        //     border: '1px solid red',
-        //     color: 'black',
-        //     fontSize: '1.2em',
-        //     marginLeft: '40%',
-        //     marginRight: '10px',
-        //     top: '70%',
-        //     width: '100px',
-        //     outline: 'none !important',
-        //     boxShadow: 'none !important',
-        // }
-        // const styleHomeButton2 = {
-        //     border: '1px solid red',
-        //     color: 'black',
-        //     fontSize: '1.2em',
-        //     top: '70%',
-        //     width: '100px',
-        //     outline: 'none !important',
-        //     boxShadow: 'none !important',
-        // }
+       
        /*  const styleHome = {
             border: '1px solid green',
             display: 'inline-block',
@@ -42,19 +23,6 @@ class NavBarTitle extends Component {
             boxShadow: 'none !important',
         } */
         
-        const styleGithub = {
-            border: '1px solid orange',
-            display: 'inline-block',
-            position: 'relative',
-            color: 'black',
-            fontSize: '1.2em',
-            // marginLeft: '50%',
-            top: '70%',
-            width: '100px',
-            outline: 'none !important',
-            boxShadow: 'none !important',
-        }
-
         return (
             <div style={{ display: 'block', width: '100%' }}>
                 {/* <Navbar className={classes.style} bg="" expand="lg" color=" #DDC59D"> */}
